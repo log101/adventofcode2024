93d419d2eb12f22c3f2f52592ca77578770f4b74
Furkan Erdem <furkan.erdem@jekirdek.com>
2024-12-04 10:47:34 +0300

    4. günü ekle

---
+++ gun4.el
@@ -0,0 +1,7 @@
+;; XMAS ve SAMX re-search-forward ve re-search-backward ile tespit edilebilir.
+;; Dikeyler transpose ile tespit edilebilir.
+;; Çaprazlar için metni çevirecek bir matematiksel bir fonksiyon bulabilirim.
+;; X S X       X S       S
+;; M A  M     M   A     A
+;; A M   A   A     M   M
+;; S X    S S       X X
